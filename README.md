255621d925cf5ca4ad99409a0abb1a1d5a37ab12
Maicon Campbell <maicon_campbell@hotmail.com>
2022-02-04 22:12:25 -0300

    Surpresinha Mega - (Front e back)

---
+++ README.md
@@ -0,0 +1,17 @@
+ # Projeto Surpresinha da Mega 💻
+ 
+## Bibliotecas
+
+ - React
+ - FlexBox
+
+## Motivação
+  Criar uma app WEB para manipular renderização randômica de acordo com o estado
+  do componente input, a geração acontece quando clicarmos no botão gerar número aleatório
+  podemos manipular o campo de input de 6 a 15 números.
+
+<div>
+  <p style='font-size: 22px'>
+  "Já experimentou acreditar em você? Tente... Você não faz ideia do que é capaz!"
+  </p>
+<div>
---
+++ src/App.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import './App.css'
+
+const App = () => {
+  const [qtd, setQtd] =  useState(6);
+  const numSorteio = Array(qtd || 6);
+  const [sorteio, setSorteio] = useState(numSorteio);
+
+  function gerarNumeroNaoContido() {
+    // Math.random() * (max - min) + min
+    const numAleatorio = parseInt(Math.random() * (61 - 1)) + 1;
+
+    return numSorteio.includes(numAleatorio) ?
+    gerarNumeroNaoContido() : 
+    numAleatorio
+  }
+
+  function gerarNumeros() {
+    console.log(qtd)
+    const numeros = Array(+qtd)
+    .fill(0)
+    .reduce(nums => {
+      const novoNumero = gerarNumeroNaoContido()
+      return [...nums, novoNumero]
+    }, [])
+    .sort((n1, n2) => n1 - n2)
+
+    return numeros;
+  }
+
+  return (
+    <div className='container'>
+      <div className='container-sorteio'>
+        <h1>Mega sena - surpresinha</h1>
+
+        <div className='resultado'>
+          {
+            sorteio.length >= 6 ?
+              sorteio.map((item, i) => {
+                return (
+                  <div className='numeroSorteado' key={i}>
+                    <strong className='numero'>{item}</strong>
+                  </div>
+                )
+              })
+            :
+              undefined
+          }
+        </div>
+
+          <form action="#">
+            <label className='label-form' htmlFor="qtdNumeros">Informar a quantidade de números:</label>
+            <input
+              name="qtdNumeros"
+              id='input'
+              type='number'
+              value={qtd}
+              min={6}
+              max={15}
+              onChange={e => setQtd(e.target.value)}
+            />
+          </form>
+
+          <div className='containerBtns'>
+            <button className='gerarNumero' onClick={() => {setSorteio(gerarNumeros())}}>Gerar Número</button>
+            <button className='limpar' onClick={_ => setSorteio([])}>Limpar</button>
+          </div>
+        </div>
+      </div>
+    )
+}
+
+export default App;
